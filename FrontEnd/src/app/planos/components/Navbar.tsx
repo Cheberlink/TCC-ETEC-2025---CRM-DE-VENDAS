af4f5820eb31ee36fbe245fe57d644913dcaf272
Cheberlink <briancheberle@gmail.com>
2025-08-28 12:37:13 -0300

    teste

--- FrontEnd/src/app/planos/components/Navbar.tsx
+++ FrontEnd/src/app/planos/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <button className="text-white w-auto p-7 font-bold text-[1.4rem]">Produtos</button>
             <button className="text-white w-auto p-7 font-bold text-[1.4rem]">Recursos</button>
             <button className="text-white w-auto p-7 font-bold text-[1.4rem]">Preços</button>
-            <button className="text-white w-auto p-7 font-bold text-[1.4rem] ml-200">Assine Agora</button>
+            <button className="text-white w-auto p-7 font-bold text-[1.4rem] ml-200">Assine Agora!</button>
             <button className="text-white w-auto py-2 px-4  font-bold text-[1.1rem] bg-black">Registre-se</button>
         </nav>
        
