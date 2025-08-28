ec053543dcc0905c71e954e2b1310c4633a814f3
Cheberlink <briancheberle@gmail.com>
2025-08-28 12:34:47 -0300

    Começando a pagina planos

---
+++ FrontEnd/src/app/planos/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+
+
+const Page: React.FC = () => {
+  return (
+    <div className='flex w-screen h-screen'>
+        <Navbar/>
+    </div>
+  );
+};
+
+export default Page;
