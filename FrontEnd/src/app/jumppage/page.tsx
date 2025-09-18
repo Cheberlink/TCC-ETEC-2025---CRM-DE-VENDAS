3061823bd876a81882acccb5f783ba7f8b01ce3e
Cheberlink <briancheberle@gmail.com>
2025-09-18 09:49:35 -0300

    Atualizando paginas 1.0

--- FrontEnd/src/app/jumppage/page.tsx
+++ FrontEnd/src/app/jumppage/page.tsx
@@ -4,34 +4,26 @@ import Header from "../../components/header";
 
 const Page: React.FC = () => {
   return (
-    <div className="min-h-screen bg-white text-black font-sans">
+    <div className="min-h-screen bg-[#360D15] text-white font-sans">
 
      <Header type={1}></Header>
       {/* Hero Section */}
-      <section className="px-6 md:px-20 py-20">
-        <h1 className="text-4xl md:text-5xl font-bold mb-8 max-w-3xl">
-          O melhor software <br />
-          para interação com o <br />
-          cliente
-        </h1>
+      
+       <div className="flex p-30 justify-center h-screen">
+       <p className='mb-8 text-center'>
+         <h1 className="text-4xl md:text-5xl font-bold mb-8 max-w-3xl">
+            Impulsione suas vendas  <br />
+            Com um CRM simples e poderoso <br />
+          </h1>
+          <br/>
+          <h2 className='mb-8 text-xl'>
+            Centralize seus leads, acompanhe funis de vendas e aumente sua produtividade.
+          </h2>
+       </p>
 
-       <div className="flex flex-row gap-6 mt-6">
-  <button className="bg-[#d4d9e2] text-[#BD1D1D] font-bold px-6 py-4 border border-transparent
-  hover:bg-[#9FA2A9] hover:shadow-lg transition-all duration-300 active:bg-white">
-  Teste 14 dias <br /> GRÁTIS
-</button>
-
-
-
-
-  <button className="bg-[#d4d9e2] text-[#BD1D1D] font-bold px-6 py-4 border border-transparent
-  hover:bg-[#9FA2A9] hover:shadow-lg transition-all duration-300 active:bg-white">
-    Fale com um <br /> especialista
-  </button>
+       <button></button>
+ 
 </div>
-
-
-      </section>
 
     </div>
   );
--- FrontEnd/src/app/planos/page.tsx
+++ FrontEnd/src/app/planos/page.tsx
@@ -8,8 +8,13 @@ const Page: React.FC = () => {
 
       <main className="flex justify-center space-x-8 py-12">
         {/* Básico */}
-        <div className="bg-gray-400 w-64 h-72 p-6 flex flex-col justify-between rounded-md">
+        <div className="bg-gray-400 w-64 h-120 p-6 flex flex-col justify-between rounded-md">
           <h2 className="text-white text-xl font-semibold">Básico</h2>
+          <p className="mb-8 text-white">
+            Praticidade pelo preço
+            <br/>
+            mais acessivel
+          </p>
           <div>
             <p className="text-white text-3xl font-light">39 BRL</p>
             <p className="text-white text-sm">por usuário/mês</p>
@@ -20,9 +25,9 @@ const Page: React.FC = () => {
         </div>
 
         {/* Avançado */}
-        <div className="bg-red-900 w-64 h-72 p-6 flex flex-col justify-between rounded-md text-white">
+        <div className="bg-red-900 w-64 h-120 p-6 flex flex-col justify-between rounded-md text-white">
           <h2 className="text-xl font-semibold">Avançado</h2>
-          <p className="mb-8">
+          <p className="mb-8"> 
             Faça ainda mais vendas
             <br />
             com bots e automações
@@ -37,7 +42,7 @@ const Page: React.FC = () => {
         </div>
 
         {/* Empresarial */}
-        <div className="bg-black w-64 h-72 p-6 flex flex-col justify-between rounded-md text-white">
+        <div className="bg-black w-64 h-120 p-6 flex flex-col justify-between rounded-md text-white">
           <h2 className="text-xl font-semibold">Empresarial</h2>
           <p className="mb-8">
             Organize grandes
