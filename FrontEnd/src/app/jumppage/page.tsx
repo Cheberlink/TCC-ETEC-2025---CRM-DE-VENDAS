ccb3a9958cb82659ddf3282e7845b7d3c572868f
Cheberlink <briancheberle@gmail.com>
2025-10-01 18:29:04 -0300

    Finalizando a jumppage

--- FrontEnd/src/app/jumppage/page.tsx
+++ FrontEnd/src/app/jumppage/page.tsx
@@ -4,7 +4,7 @@ import Header from "../../components/header";
 
 const Page: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#360D15] text-white font-sans">
+    <div className="min-h-screen bg-gradient-to-t from-[#831625] to-[#21080D] text-white font-sans">
       {/* Header */}
       <Header type={1} />
 
@@ -20,13 +20,13 @@ const Page: React.FC = () => {
           produtividade.
         </h2>
 
-        <button className="bg-red-900 text-white px-4 py-2 rounded hover:bg-white hover:text-red-900 transition-all duration-200">
+        <button className="bg-red-900 text-white px-4 py-2 rounded hover:bg-white hover:text-red-900 transition-all duration-200 active:bg-[#585858]">
           Teste grátis por 14 dias
         </button>
       </div>
 
     
-      <div className="flex flex-col md:flex-row justify-center items-center gap-6 px-10 py-20 text-black items-stretch">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-6 px-10 py-20 text-black items-stretch ">
         
         <div className="bg-white rounded-lg shadow-md p-6 text-center w-70 h-50">
           <div className="text-red-900 text-5xl mb-4">⚙️</div>
@@ -43,6 +43,58 @@ const Page: React.FC = () => {
           <h3 className="font-semibold text-lg">Relatórios inteligentes</h3>
         </div>
       </div>
+      <main className="flex justify-center space-x-8 py-12 ">
+        {/* Básico */}
+        <div className="bg-gray-400 w-70 h-120 p-6 flex flex-col justify-between rounded-md">
+          <h2 className="text-white text-xl font-semibold">Básico</h2>
+          <p className="mb-8 text-white">
+            Praticidade pelo preço
+            <br/>
+            mais acessivel
+          </p>
+          <div>
+            <p className="text-white text-3xl font-light">39 BRL</p>
+            <p className="text-white text-sm">por usuário/mês</p>
+          </div>
+          <button className="bg-white text-red-900 font-semibold py-2 px-4 rounded hover:bg-[#DFDFDF] hover:rounded-lg hover:shadow-md transition-all duration-200 active:bg-[#696969]">
+            Teste Grátis
+          </button>
+        </div>
+
+        {/* Avançado */}
+        <div className="bg-red-900 w-70 h-120 p-6 flex flex-col justify-between rounded-md text-white">
+          <h2 className="text-xl font-semibold">Avançado</h2>
+          <p className="mb-8"> 
+            Faça ainda mais vendas
+            <br />
+            com bots e automações
+          </p>
+          <div>
+            <p className="text-3xl font-light">79 BRL</p>
+            <p className="text-sm">por usuário/mês</p>
+          </div>
+          <button className="bg-white text-red-900 font-semibold py-2 px-4 rounded hover:bg-[#DFDFDF] hover:rounded-lg hover:shadow-md transition-all duration-200 active:bg-[#696969]">
+            Teste Grátis
+          </button>
+        </div>
+
+        {/* Empresarial */}
+        <div className="bg-black w-70 h-120 p-6 flex flex-col justify-between rounded-md text-white">
+          <h2 className="text-xl font-semibold">Empresarial</h2>
+          <p className="mb-8">
+            Organize grandes
+            <br />
+            quantidades de leads e dados
+          </p>
+          <div>
+            <p className="text-3xl font-light">99 BRL</p>
+            <p className="text-sm">por usuário/mês</p>
+          </div>
+          <button className="bg-white text-red-900 font-semibold py-2 px-4 rounded hover:bg-[#DFDFDF] hover:rounded-lg hover:shadow-md transition-all duration-200 active:bg-[#696969]">
+            Teste Grátis
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
